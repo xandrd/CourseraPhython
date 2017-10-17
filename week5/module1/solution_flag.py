b72bbe5cc363af4723c3693a999e0a3b36eedd10
Alexander Drozdov <adrozdov@ucla.edu>
2017-10-17 12:52:57 -0700

    week5 module1

--- week5/module1/solution_flag.py
+++ week5/module1/solution_flag.py
@@ -1,23 +1,20 @@
 n = int(input())
-f = ('+___ ', '|__\ ', '|    ')
+for i in range(1, n+1):
+    print('+___', end=' ')
+
+print()
 
 for i in range(1, n+1):
-    print(f[0], end=' ')
+    print('|' + str(i) + ' /', end=' ')
 
-print('')
-
-
-for i in range(1, n+1):
-    print('|' + str(i) + ' / ', end=' ')
-
-print('')
+print()
 
 for i in range(1, n+1):
-    print(f[1], end=' ')
+    print('|__\\', end=' ')
 
-print('')
+print()
 
 for i in range(1, n+1):
-    print(f[2], end=' ')
+    print('|   ', end=' ')
 
-print('')
+print()
