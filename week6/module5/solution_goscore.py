7f3e3560f9c55deaa3baa554ff88c9929eb753db
Alexander Drozdov <adrozdov@ucla.edu>
2017-10-25 13:04:00 -0700

    goscore fixed

--- week6/module5/solution_goscore.py
+++ week6/module5/solution_goscore.py
@@ -1,5 +1,3 @@
-from gc import enable
-
 fid = open('input.txt', 'r', encoding='utf8')
 arr = []
 min_scr = 40
@@ -16,30 +14,37 @@ fid.close()
 got_spot = 0
 score_spot = 0
 current_scr = -1
-if len(arr) != max_spot:
+score_cnt = []
+if len(arr) > max_spot:
     arr.sort(reverse=True)
     max_scr = max(arr)
     previous_scr = max(arr)
     current_scr = max(arr)
 
-    for i in range(len(arr)):
-        if arr[i] == current_scr and got_spot <= max_spot:
-            got_spot += 1
-        elif arr[i] < current_scr and got_spot <= max_spot:
-            previous_scr = current_scr
-            current_scr = arr[i]
-            got_spot += 1
+    idx = -1
+    for a in arr:
+        if len(score_cnt) > 0 and score_cnt[idx][0] == a:
+            score_cnt[idx][1] += 1
+        else:
+            score_cnt.append([a, 1])
+            idx += 1
 
-        if got_spot > max_spot:
-            current_scr = previous_scr
+    for a in score_cnt:
+        if got_spot+a[1] <= max_spot:
+            got_spot += a[1]
+            current_scr = a[0]
+        else:
+            break
 
-    if (current_scr == max_scr) and (got_spot > max_spot):
+    if got_spot == 0:
         current_scr = 1
+
+
 else:
     current_scr = 0
 
-print(*arr)
-print(current_scr, max_spot, got_spot)
-#fid = open('output.txt', 'w', encoding='utf8')
-#print(current_scr, file=fid)
-#fid.close()
+# print(score_cnt)
+# print(current_scr, max_spot, got_spot)
+fid = open('output.txt', 'w', encoding='utf8')
+print(current_scr, file=fid)
+fid.close()
